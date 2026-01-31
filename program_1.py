454c3b312e5d939dde86a6b873ca1dfcd25dbb22
avk0815 <avkushneryk@students.unwsp.edu>
2026-01-30 23:57:51 -0600

    assignment

--- program_1.py
+++ program_1.py
@@ -1,6 +1,12 @@
 def personal_information():
-    print('...')
-
+    name = "Mario"
+    address = "1708 Mario Kart drive, st. paul, MN 55091"
+    phone = "(651) 911-4167"
+    major = "Biomedical Engineering"
+    print('Name:', name)
+    print('Address:', address)
+    print('Phone:', phone)
+    print('Major:', major)
 
 # Line which calls the above function.
 personal_information()
